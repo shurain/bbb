d1db2abaf78e15dfbcf1836d15b120435154e976
Sungjoo Ha <shurain@gmail.com>
2012-05-17 00:30:24 +0900

    Removing comments

--- learn_board.py
+++ learn_board.py
@@ -87,14 +87,6 @@ classes = {
         }
 
 
-def print_image(im, c):
-    for y in range(8):
-        for x in range(8):
-            GRID = (40 * x, 40 * y, 40 * (x + 1), 40 * (y + 1))
-            grid = im.crop(GRID)
-            grid.save(str(c[8 * y + x]) + "_" + str(int(time.time())) + '.png')
-
-
 def parse_data(im, c):
     X = []
     Y = []
@@ -127,17 +119,6 @@ def parse_data(im, c):
     return X, Y
 
 
-def learn_board(X, Y):
-    clf = svm.SVC()
-    clf.fit(X, Y)
-    return clf
-
-
-def test_board(clf, X):
-    result = clf.predict(X)
-    return result
-
-
 def run(PATH):
     data = []
     for root, dirs, files in os.walk(PATH):
@@ -152,18 +133,10 @@ def run(PATH):
                     d.append(row.split())
                 data.append((im, d))
 
-    #count = 0
     train_X = []
     train_Y = []
-    #test_images = ''
     for im, dat in data:
-        #if count == 0:
-        #    test_images = im
-        #    test_X, test_Y = parse_data(im, dat)
-        #    count += 1
-        #    continue
         temp_X, temp_Y = parse_data(im, dat)
-        #test_images = im
         train_X += temp_X
         train_Y += temp_Y
 
@@ -172,25 +145,7 @@ def run(PATH):
     b.learn(train_X, train_Y)
     b.dump(f)
 
-    #clf = learn_board(train_X, train_Y)
-    #f = open('trained', 'w')
-    #pickle.dump(clf, f)
-    #print b.get_board(test_images)
-    #print [COLORS[int(x)] for x in test_Y]
-    #print [COLORS[int(x)] for x in temp_Y]
-    #clf = pickle.load(f)
-    #prediction = test_board(clf, test_X)
-
-    #print_image(test_images, prediction)
-
 
 if __name__ == '__main__':
     PATH = 'learn'
     run(PATH)
-
-    #f = open('trained', 'r')
-    #b = Board()
-    #b.load(f)
-
-    #image = Image.open('snapshot_1337115240.png')
-    #print b.get_board(image)
